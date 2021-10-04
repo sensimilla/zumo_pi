cb9d9b516d03c2deb9750a8609de0d90a2708cf1
Richard <richardeyton@hoottmail.com>
2021-10-04 16:07:34 +0100

    added threading and queue size of 1

--- scripts/amg8833_node.py
+++ scripts/amg8833_node.py
@@ -6,16 +6,19 @@ from std_srvs.srv import Trigger
 import busio
 import board
 import adafruit_amg88xx
+import threading
 
 # TODO switch to Seeed amg library, don't trust adafruit one
 # TODO add rate setting to start service
 
 class AMG8833DriverROSWrapper:
     def __init__(self):
+        self.lock = threading.Lock()
+        self.publishThread = None
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.amg = adafruit_amg88xx.AMG88XX(self.i2c)
 
-        self.amgPub = rospy.Publisher('~amg8833_raw', Amg8833Raw, queue_size=5)
+        self.amgPub = rospy.Publisher('~amg8833_raw', Amg8833Raw, queue_size=1)
 
         rospy.Service("~start_amg833", StartAmg8833, self.callback_start_amg)
         rospy.Service("~stop_amg833", Trigger, self.callback_stop_amg)
@@ -28,7 +31,7 @@ class AMG8833DriverROSWrapper:
 
         self.is_enabled = False
 
-        self.publish_timer = None
+        #self.publish_timer = None
 
         if autostart:
             rospy.loginfo("Autostarting AMG8833 8x8 IR Sensor")
@@ -37,26 +40,27 @@ class AMG8833DriverROSWrapper:
     def start_amg(self):
         self.is_enabled = True
         period = rospy.Duration(1.0/self.rate)
-        self.publish_timer = rospy.Timer(period, self.read_amg)
+        self.publishThread = rospy.Timer(period, self.read_amg)
 
         rospy.loginfo("Started Reading AMG8833 8x8 IR Sensor ")
         rospy.loginfo("Target rate " + str(self.rate) + " Hz" )
 
     def stop_amg(self):
-        self.publish_timer.shutdown()
+        self.publishThread.shutdown()
         self.is_enabled = False
         rospy.loginfo("AMG8833 Reading Stopped")
 
     def read_amg(self, event=None):  # called from timer loop which adds event paramter
-        flat_pixels = []
-        try:
-            for row in self.amg.pixels:
-                for temp in row:
-                    flat_pixels.append(temp)
-            chip_temp = self.amg.temperature
-        except:
-            rospy.logerr("failed to read AMG833")
-            return
+        with self.lock:
+            flat_pixels = []
+            try:
+                for row in self.amg.pixels:
+                    for temp in row:
+                        flat_pixels.append(temp)
+                chip_temp = self.amg.temperature
+            except:
+                rospy.logerr("failed to read AMG833")
+                return
 
         amgReading = Amg8833Raw()
         amgReading.header.frame_id = rospy.get_param("~frame_id", "amg8833_link")
@@ -67,7 +71,7 @@ class AMG8833DriverROSWrapper:
         self.amgPub.publish(amgReading)
 
     def stop(self):
-        self.publish_timer.shutdown()
+        self.publishThread.shutdown()
         rospy.loginfo("AMG833 Closed")
 
     def set_rate(self, new_rate):
